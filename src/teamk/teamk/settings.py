058369dff0464481502756e35409da0512aa84b2
zoegerolemou <2023949g@student.gla.ac.uk>
2015-01-06 00:42:13 +0200

    Setting up the Django Application for the Booking Management System

--- src/teamk/teamk/settings.py
+++ src/teamk/teamk/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking_system
+    'booking_system'
 )
 
 MIDDLEWARE_CLASSES = (
