6af5ca4db0355a353f75ac9ea370b4bdddc91373
zoegerolemou <2023949g@student.gla.ac.uk>
2015-01-06 01:12:03 +0200

    Setting up the Templates

--- src/teamk/booking_system/urls.py
+++ src/teamk/booking_system/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from booking_system import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'))﻿
+	url(r'^$', views.index, name='index'))
