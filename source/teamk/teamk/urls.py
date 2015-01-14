aad9884dacb24167b5b52bf00193f31140394697
Zoe Gerolemou <2023949g@bo720-1-05.(none)>
2015-01-14 16:23:54 +0000

    Renamed the python django soure files in order to start the project setup from scratch

---
+++ source/teamk/teamk/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'teamk.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^booking_system/', include('booking_system.urls')),
+)
