9ab6d2294aaca85f77af5e738450efe0608d95f6
Zoe Gerolemou <2023949g@bo720-7-01.(none)>
2015-01-15 17:24:28 +0000

    Uploaded attendance page for Coaches

--- src/teamk_project/bookingsystem/urls.py
+++ src/teamk_project/bookingsystem/urls.py
@@ -3,5 +3,7 @@ from bookingsystem import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(r'^sessions', views.sessions, name ='sessions'),
+	url(r'^index', views.index, name='index'),
+	url(r'^sessions', views.sessions, name='sessions'),
+	url(r'^attendance', views.attendance, name='attendance'),
 	url(r'^editProfile', views.editProfile, name='editProfile'))
--- src/teamk_project/bookingsystem/views.py
+++ src/teamk_project/bookingsystem/views.py
@@ -12,6 +12,11 @@ def sessions(request):
 	context_dict={}
 	return render_to_response('bookingsystem/sessions.html', context_dict, context)
 
+def attendance(request):
+	context = RequestContext(request)
+	context_dict={}
+	return render_to_response('bookingsystem/attendance.html', context_dict, context)
+
 def editProfile(request):
 	context = RequestContext(request)
 	context_dict={}
