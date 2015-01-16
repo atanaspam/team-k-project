18d854d4582459c9729c4cd7369d4c27bf6a6b4c
Atanas Pamukchiev <atanas.pam@gmail.com>
2015-01-16 01:41:49 +0000

    Trying to fix this

--- src/teamk_project/manager/urls.py
+++ src/teamk_project/manager/urls.py
@@ -5,13 +5,10 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^index', views.index, name='index'),
 	url(r'^bookings', views.bookings, name='bookings'),
-<<<<<<< HEAD
 	url(r'^coaches', views.coaches, name='coaches')
 )
-=======
-	url(r'^coaches', views.coaches, name='coaches'),
-	url(r'^coachProfile', views.coachProfile, name='coachProfile'),
-	url(r'^members', views.members, name='members'),
-	url(r'^confirmbooking', views.confirmbooking, name='confirmbooking'),
-	url(r'^audit', views.audit, name='audit'))
->>>>>>> 44dba765de46bf369610a1855c4353354151bda0
+	# url(r'^coaches', views.coaches, name='coaches'),
+	# url(r'^coachProfile', views.coachProfile, name='coachProfile'),
+	# url(r'^members', views.members, name='members'),
+	# url(r'^confirmbooking', views.confirmbooking, name='confirmbooking'),
+	# url(r'^audit', views.audit, name='audit'))
