68c0dbbea2f1576fd031d7e5dd7139984bf34543
Pedro Henrique de Oliveira Quintas <phoquintas@gmail.com>
2015-01-18 22:19:54 +0000

    updated members list
    fixed a few bugs

--- Website Proto Parent/js/scripts.js
+++ Website Proto Parent/js/scripts.js
@@ -1,6 +1,30 @@
-
 $(document).ready(function(){
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
+
   });
 });
+
+function populateChildCheckboxes()
+{
+	var div = document.createElement('div');
+	div.class = checkbox;
+	div.id = "id";
+
+	var checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
+	checkbox.class = "checkbox";
+	checkbox.name = "name";
+	checkbox.value = "value";
+	checkbox.id = "id";
+
+	var label = document.createElement('label')
+	label.htmlFor = "id";
+	label.appendChild(document.createTextNode('text for label after checkbox'));
+
+	div.appendChild(checkbox);
+	div.appendChild(label);
+
+	childrenCheckboxes.appendChild(div);
+}
+
