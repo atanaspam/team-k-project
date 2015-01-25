7077964264ec2dc25e46cec9f4e3897d827e8dc9
Pedro Henrique de Oliveira Quintas <phoquintas@gmail.com>
2015-01-25 18:55:30 +0000

    updated css and html parent
    changed the look of the login and parent buttons

--- Website Proto Manager/js/scripts.js
+++ Website Proto Manager/js/scripts.js
@@ -2,5 +2,13 @@
 $(document).ready(function(){
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
+
+    generateCheckBoxes();
+
   });
 });
+
+function generateCheckBoxes(){
+
+	alert("PATOS no AR");
+}
