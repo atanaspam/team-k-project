1697ae80135297907a421d46bd75c0b1dd036415
kadrouven <ka.drouven@gmail.com>
2015-01-29 13:24:49 +0000

    Print feature + tidy up

--- src/teamk_project/static/js/scripts.js
+++ src/teamk_project/static/js/scripts.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+function printContent(el){
+	var restorepage = document.body.innerHTML;
+	var printcontent = document.getElementById(el).innerHTML;
+	document.body.innerHTML = printcontent;
+	window.print();
+	document.body.innerHTML = restorepage;
+}
