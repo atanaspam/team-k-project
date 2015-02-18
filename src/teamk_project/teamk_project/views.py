6aabc4808a8b04e8f8e3b118fefab254781e629c
Atanas Pamukchiev <atanas.pam@gmail.com>
2015-02-18 19:27:29 +0000

    Fixed the registration process

--- src/teamk_project/teamk_project/views.py
+++ src/teamk_project/teamk_project/views.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from bookingsystem.forms import RegisterForm
+from django.contrib.auth.models import User
 
 def login(request):
     if request.user.is_authenticated():
@@ -67,8 +68,11 @@ def register(request):
         if form.is_valid():
             user=form.save(commit=False)
             #user.blockid = getLastBlockID()
-            # Now save to the DB block.save()
-            print user.username
+            newUser = User.objects.create_user(user.username, user.email, user.password)
+            newUser.first_name = user.first_name
+            newUser.last_name = user.last_name
+            newUser.save()
+            #   print user.username
             # Redirect on success
             return redirect('/success.html')
         else:
