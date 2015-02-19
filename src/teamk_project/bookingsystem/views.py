6483c00ab44651eeda5a1f0a3bfb58425ded6581
dkiker <2014573k@student.gla.ac.uk>
2015-02-19 18:20:38 +0000

    removed lines from merging
    —

--- src/teamk_project/bookingsystem/views.py
+++ src/teamk_project/bookingsystem/views.py
@@ -444,10 +444,6 @@ def childProfile(request, id):
 		context_dict['sessions'] = sessions
  		context_dict['child'] = child
  	return render_to_response('parent/childProfile.html', context_dict, context)
-<<<<<<< HEAD
->>>>>>> FETCH_HEAD
-=======
->>>>>>> FETCH_HEAD
 
 @login_required
 @user_passes_test(is_parent)
