29505dd0d25524a4a0b1459c30e2b791bdca92d2
Atanas Pamukchiev <atanas.pam@gmail.com>
2015-03-04 14:53:14 +0000

    Modified some of the tests

--- src/teamk_project/bookingsystem/tests.py
+++ src/teamk_project/bookingsystem/tests.py
@@ -7,6 +7,8 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from bookingsystem.models import Client
+from django.contrib.auth.models import User, Group, UserManager
+import datetime
 
 
 class SimpleTest(TestCase):
@@ -18,10 +20,21 @@ class SimpleTest(TestCase):
 
 class TestMemberStatusChange(TestCase):
     def setUp(self):
-        Client.objects.create(uid=1, firstname="Atanas", lastname = "Pamukchiev", email="atanas.pam@gmail.com", telephone="021312213", age=21, ismember=False, managedby=NULL, belongsto=1, genderid=1, experiencelevel=8)
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
+        parentsGroup = Group.objects.create(name="Parent")
+
+        parent = User.objects.create_user(username="Parent1", email="parent@parents.com", password="There is no pass")
+        parent.first_name = "John"
+        parent.last_name = "Hyfig"
+        g = Group.objects.get(name='Parent')
+        g.user_set.add(parent)
+        parent.save()
+
+        Client.objects.create(uid=1, firstname="Atanas", lastname = "Pamukchiev", email="atanas.pam@gmail.com", telephone="021312213", dateofbirth=datetime.date(1993, 10, 9), ismember=False, managedby=0, belongsto=parent, genderid=1, experiencelevel=8)
+        Client.objects.create(uid=2, firstname="Mahsa", lastname = "Johnson", email="mahsa.johnson@gmail.com", telephone="034672213", dateofbirth=datetime.date(1998, 8, 15), ismember=False, managedby=0, belongsto=parent, genderid=0, experiencelevel=2)
+
+    def test_status_of_member_change(self):
+        """Clients can be assigned as members pf the club"""
         client = Client.objects.filter(uid=1)
         client.ismember=True
         self.assertEqual(client.ismember, True)
