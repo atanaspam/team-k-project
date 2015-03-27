be65d81f3430765747ff5a601b7d3a12e1274372
kadrouven <ka.drouven@gmail.com>
2015-03-27 14:59:28 +0000

    More comments

--- src/teamk_project/bookingsystem/models.py
+++ src/teamk_project/bookingsystem/models.py
@@ -20,6 +20,43 @@ from django.db.models import Min
 from django.contrib.auth.models import User
 import datetime
 
+TYPE_CHOICES = (
+	('Morning', 'Morning'),
+	('Afternoon', 'Afternoon'),
+	('Week', 'Week'),
+	('Season', 'Season'),
+)
+
+GENDER_CHOICES = (
+	(1, 'Male'),
+	(0, 'Female')
+	)
+
+DURATION_TYPES = (
+		('0:30', '30 minutes'),
+		('1:00', '1 hour'),
+		('1:30', '1:30 hours'),
+		('2:00', '2 hours'),
+		('-1', 'Other'),
+		)
+
+@property
+def nextArrivalDate(self):
+	return self.userselectssession_set.filter(session_sessionid__begintime__gte=datetime.datetime.now()).aggregate(Min('session_sessionid__begintime')).get('session_sessionid__begintime__min')
+
+def __unicode__(self):
+	return "%s %s" % (self.firstname, self.lastname)
+class Meta:
+	db_table = 'client'
+
+class ClientManager(models.Manager):
+	def getMales(self):
+		return self.filter(genderid = 1)
+	def getFemales(self):
+		return self.filter(genderid = 0)
+
+# User
+
 class Address(models.Model):
 	addressid = models.IntegerField(primary_key=True, db_column='addressID') # Field name made lowercase.
 	city = models.CharField(max_length=45, blank=True)
@@ -30,36 +67,21 @@ class Address(models.Model):
 	class Meta:
 		db_table = 'address'
 
-TYPE_CHOICES = (
-	('Morning', 'Morning'),
-	('Afternoon', 'Afternoon'),
-	('Week', 'Week'),
-	('Season', 'Season'),
-)
-
-class Block(models.Model):
-	blockid = models.IntegerField(primary_key=True, db_column='BlockID') # Field name made lowercase.
-	begindate = models.DateField(db_column='beginDate') # Field name made lowercase.
-	enddate = models.DateField(db_column='endDate') # Field name made lowercase.
-	label = models.CharField(max_length=45, blank=True)
-	type = models.CharField(max_length=45, choices = TYPE_CHOICES)
+class UserSelectsSession(models.Model):
+	user_uid = models.ForeignKey(Client, db_column='User_uID') # Field name made lowercase.
+	session_sessionid = models.ForeignKey(Session, db_column='Session_sessionID') # Field name made lowercase.
+	status = models.CharField(max_length=1, blank=True)
+	hasattended = models.IntegerField(db_column='hasattended')
 	class Meta:
-		db_table = 'block'
-	def __str__(self):
-		return '%s %s %s' % (self.begindate, self.enddate, self.label)
+			unique_together = (("user_uid", "session_sessionid"),)
+			db_table = 'user_selects_session'
 
-class BtmRank(models.Model):
-	uid = models.IntegerField(primary_key=True, db_column='uID') # Field name made lowercase.
-	membershipnum = models.IntegerField(null=True, db_column='membershipNum', blank=True) # Field name made lowercase.
-	numofponts = models.IntegerField(null=True, db_column='numOfPonts', blank=True) # Field name made lowercase.
-	class Meta:
-		db_table = 'btm_rank'
+class AdditionalInfo(models.Model):
+	user = models.OneToOneField(User)
+	telephone = models.IntegerField(max_length=12)
 
-GENDER_CHOICES = (
-	(1, 'Male'),
-	(0, 'Female')
-	)
-
+# Client - Child    
+    
 class Client(models.Model):
 	uid = models.AutoField(primary_key=True, db_column='uID') # Field name made lowercase.
 	firstname = models.CharField(max_length=45, db_column='firstName', blank=True) # Field name made lowercase.
@@ -73,27 +95,84 @@ class Client(models.Model):
 	genderid = models.IntegerField(null=True, db_column='genderID', blank=True, choices = GENDER_CHOICES) # Field name made lowercase.
 	experiencelevel = models.IntegerField(db_column='experienceLevel') # Field name made lowercase.
 
-	@property
-	def nextArrivalDate(self):
-		return self.userselectssession_set.filter(session_sessionid__begintime__gte=datetime.datetime.now()).aggregate(Min('session_sessionid__begintime')).get('session_sessionid__begintime__min')
-
-	def __unicode__(self):
-		return "%s %s" % (self.firstname, self.lastname)
-	class Meta:
-		db_table = 'client'
-
-class ClientManager(models.Manager):
-	def getMales(self):
-		return self.filter(genderid = 1)
-	def getFemales(self):
-		return self.filter(genderid = 0)
-
-
 class Experiencelevel(models.Model):
 	levelid = models.IntegerField(primary_key=True, db_column='levelID') # Field name made lowercase.
 	label = models.CharField(max_length=45, blank=True)
 	class Meta:
 		db_table = 'experiencelevel'
+
+class Medicalcondition(models.Model):
+	ownerid = models.IntegerField(primary_key=True, db_column='ownerID') # Field name made lowercase.
+	condition = models.CharField(max_length=45, blank=True)
+	class Meta:
+		db_table = 'medicalcondition'
+    
+# Coach
+
+class sessionCoachedBy(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='id')
+    session_id = models.ForeignKey(Session, db_column='session_id')
+    user_id = models.ForeignKey(User, db_column='user_id')
+
+    class Meta:
+        db_table = 'session_coachedby'
+
+class DefaultCoaches(models.Model):
+	monMor = models.ForeignKey(User, null=True, related_name = 'MondayMorning')
+	monAft = models.ForeignKey(User, null=True, related_name = 'MondayAfternoon')
+	tueMor = models.ForeignKey(User, null=True, related_name = 'TuesdayMorning')
+	tueAft = models.ForeignKey(User, null=True, related_name = 'TuesdayAfternon')
+	wedMor = models.ForeignKey(User, null=True, related_name = 'WednesdayMorning')
+	wedAft = models.ForeignKey(User, null=True, related_name = 'WednesdayAfternoon')
+	thuMor = models.ForeignKey(User, null=True, related_name = 'ThursdayMorning')
+	thuAft = models.ForeignKey(User, null=True, related_name = 'ThursdayAfternoon')
+	friMor = models.ForeignKey(User, null=True, related_name = 'FridayMorning')
+	friAft = models.ForeignKey(User, null=True, related_name = 'FridayAfternoon')
+
+# Blocks and Sessions
+
+class Block(models.Model):
+	blockid = models.IntegerField(primary_key=True, db_column='BlockID') # Field name made lowercase.
+	begindate = models.DateField(db_column='beginDate') # Field name made lowercase.
+	enddate = models.DateField(db_column='endDate') # Field name made lowercase.
+	label = models.CharField(max_length=45, blank=True)
+	type = models.CharField(max_length=45, choices = TYPE_CHOICES)
+	class Meta:
+		db_table = 'block'
+	def __str__(self):
+		return '%s %s %s' % (self.begindate, self.enddate, self.label)
+
+class Session(models.Model):
+	sessionid = models.IntegerField(primary_key=True, db_column='sessionID') # Field name made lowercase
+	duration = models.CharField(max_length=45, blank=True, choices=DURATION_TYPES)
+	begintime = models.DateTimeField(null=True, db_column='beginTime', blank=True) # Field name made lowercase.
+	endtime = models.DateTimeField(null=True, db_column='endTime', blank=True) # Field name made lowercase.
+	block_blockid = models.ForeignKey(Block, db_column='Block_BlockID') # Field name made lowercase.
+	capacity = models.IntegerField(null=True, blank=True)
+	agegroup = models.CharField(max_length=45, db_column='ageGroup', blank=True) # Field name made lowercase.
+	skillgroup = models.CharField(max_length=45, db_column='skillGroup', blank=True) # Field name made lowercase.
+	isfull = models.CharField(max_length=45, db_column='isFull', blank=True) # Field name made lowercase.
+	coachedby = models.ManyToManyField(User, null=True, blank=True)
+	class Meta:
+			db_table = 'session'
+
+class Notes(models.Model):
+	noteid = models.IntegerField(primary_key=True, db_column='noteID') # Field name made lowercase.
+	note = models.TextField(db_column='Note', blank=True) # Field name made lowercase. This field type is a guess.
+	session_sessionid = models.IntegerField(db_column='Session_sessionID') # Field name made lowercase.
+	hasnotes = models.IntegerField(null=True, db_column='hasNotes', blank=True) # Field name made lowercase.
+	class Meta:
+		db_table = 'notes'
+
+
+# Extras or Unused
+
+class BtmRank(models.Model):
+	uid = models.IntegerField(primary_key=True, db_column='uID') # Field name made lowercase.
+	membershipnum = models.IntegerField(null=True, db_column='membershipNum', blank=True) # Field name made lowercase.
+	numofponts = models.IntegerField(null=True, db_column='numOfPonts', blank=True) # Field name made lowercase.
+	class Meta:
+		db_table = 'btm_rank'
 
 class Extras(models.Model):
 	extrasid = models.IntegerField(primary_key=True, db_column='extrasID') # Field name made lowercase.
@@ -104,19 +183,7 @@ class Extras(models.Model):
 	class Meta:
 		db_table = 'extras'
 
-class Medicalcondition(models.Model):
-	ownerid = models.IntegerField(primary_key=True, db_column='ownerID') # Field name made lowercase.
-	condition = models.CharField(max_length=45, blank=True)
-	class Meta:
-		db_table = 'medicalcondition'
-
-class Notes(models.Model):
-	noteid = models.IntegerField(primary_key=True, db_column='noteID') # Field name made lowercase.
-	note = models.TextField(db_column='Note', blank=True) # Field name made lowercase. This field type is a guess.
-	session_sessionid = models.IntegerField(db_column='Session_sessionID') # Field name made lowercase.
-	hasnotes = models.IntegerField(null=True, db_column='hasNotes', blank=True) # Field name made lowercase.
-	class Meta:
-		db_table = 'notes'
+# Payments
 
 class Payment(models.Model):
 	paymentid = models.IntegerField(primary_key=True, db_column='paymentID') # Field name made lowercase.
@@ -136,36 +203,7 @@ class Paymenttype(models.Model):
 	class Meta:
 		db_table = 'paymenttype'
 
-DURATION_TYPES = (
-		('0:30', '30 minutes'),
-		('1:00', '1 hour'),
-		('1:30', '1:30 hours'),
-		('2:00', '2 hours'),
-		('-1', 'Other'),
-		)
-
-class Session(models.Model):
-	sessionid = models.IntegerField(primary_key=True, db_column='sessionID') # Field name made lowercase
-	duration = models.CharField(max_length=45, blank=True, choices=DURATION_TYPES)
-	begintime = models.DateTimeField(null=True, db_column='beginTime', blank=True) # Field name made lowercase.
-	endtime = models.DateTimeField(null=True, db_column='endTime', blank=True) # Field name made lowercase.
-	block_blockid = models.ForeignKey(Block, db_column='Block_BlockID') # Field name made lowercase.
-	capacity = models.IntegerField(null=True, blank=True)
-	agegroup = models.CharField(max_length=45, db_column='ageGroup', blank=True) # Field name made lowercase.
-	skillgroup = models.CharField(max_length=45, db_column='skillGroup', blank=True) # Field name made lowercase.
-	isfull = models.CharField(max_length=45, db_column='isFull', blank=True) # Field name made lowercase.
-	coachedby = models.ManyToManyField(User, null=True, blank=True)
-	class Meta:
-			db_table = 'session'
-
-class UserSelectsSession(models.Model):
-	user_uid = models.ForeignKey(Client, db_column='User_uID') # Field name made lowercase.
-	session_sessionid = models.ForeignKey(Session, db_column='Session_sessionID') # Field name made lowercase.
-	status = models.CharField(max_length=1, blank=True)
-	hasattended = models.IntegerField(db_column='hasattended')
-	class Meta:
-			unique_together = (("user_uid", "session_sessionid"),)
-			db_table = 'user_selects_session'
+# Venue
 
 class Venue(models.Model):
 	venueid = models.IntegerField(primary_key=True, db_column='venueID') # Field name made lowercase.
@@ -192,28 +230,4 @@ class SubvenueUsedforSession(models.Model):
 	class Meta:
 			unique_together = (("subvenue_subvenueid", "session_sessionid"),)
 			db_table = 'subvenue_usedfor_session'
-
-class AdditionalInfo(models.Model):
-	user = models.OneToOneField(User)
-	telephone = models.IntegerField(max_length=12)
-
-class sessionCoachedBy(models.Model):
-    id = models.IntegerField(primary_key=True, db_column='id')
-    session_id = models.ForeignKey(Session, db_column='session_id')
-    user_id = models.ForeignKey(User, db_column='user_id')
-
-    class Meta:
-        db_table = 'session_coachedby'
-
-class DefaultCoaches(models.Model):
-	monMor = models.ForeignKey(User, null=True, related_name = 'MondayMorning')
-	monAft = models.ForeignKey(User, null=True, related_name = 'MondayAfternoon')
-	tueMor = models.ForeignKey(User, null=True, related_name = 'TuesdayMorning')
-	tueAft = models.ForeignKey(User, null=True, related_name = 'TuesdayAfternon')
-	wedMor = models.ForeignKey(User, null=True, related_name = 'WednesdayMorning')
-	wedAft = models.ForeignKey(User, null=True, related_name = 'WednesdayAfternoon')
-	thuMor = models.ForeignKey(User, null=True, related_name = 'ThursdayMorning')
-	thuAft = models.ForeignKey(User, null=True, related_name = 'ThursdayAfternoon')
-	friMor = models.ForeignKey(User, null=True, related_name = 'FridayMorning')
-	friAft = models.ForeignKey(User, null=True, related_name = 'FridayAfternoon')
 
